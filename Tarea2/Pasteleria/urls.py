39a470227d0a081c5b7a3a9c5b68b4ce97e31bef
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-09-13 16:57:18 -0300

    agrega el registro

--- Tarea2/Pasteleria/urls.py
+++ Tarea2/Pasteleria/urls.py
@@ -8,4 +8,7 @@ urlpatterns =[
     path('home/',views.home,name='home'),
     path('formularioCarla/',views.formularioCarla,name='formularioCarla'),
     path('formularioDiego/',views.formularioDiego,name='formularioDiego'),
+    path('registro/', views.registro, name='registro'),
+    path('registro/tryRegistrar/', views.tryRegistrar, name='tryRegistrar'),
+
 ]
--- Tarea2/Pasteleria/views.py
+++ Tarea2/Pasteleria/views.py
@@ -10,9 +10,9 @@ def login(request):
 
 def tryLogin(request):
     # if this is a POST request we need to process the form data
-    nombre=request.POST["nombre"]
-    print(nombre)
-    return render(request,'Pasteleria/home.html', {'p':nombre})
+    datos = []
+    datos.append(request.POST["nombre"])
+    return render(request,'Pasteleria/home.html', {'llaveDatos':datos})
 
 def home(request):
     return render(request,'Pasteleria/home.html')
@@ -22,3 +22,13 @@ def formularioCarla(request):
 
 def formularioDiego(request):
     return render(request,'Pasteleria/formularioDiego.html')
+
+def registro(request):
+    return render(request,'Pasteleria/registrarse.html')
+
+def tryRegistrar(request):
+    datos=[]
+    datos.append(request.POST["nombre"])
+    datos.append(request.POST["correo"])
+    datos.append(request.POST["telefono"])
+    return render(request,'Pasteleria/home.html', {'llaveDatos':datos})
